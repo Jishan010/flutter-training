7657c2be347212cca768aae9e6c03296a07c87fc
Arindom Ghosh <arindomghosh23@gmail.com>
2023-03-05 16:08:09 +0530

    sample state management examples

---
+++ lectures/flutter_notifiers/change_notifiers_example/android/app/src/main/kotlin/com/example/change_notifiers_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.change_notifiers_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lectures/flutter_notifiers/change_notifiers_example/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+// https://api.flutter.dev/flutter/foundation/ChangeNotifier-class.html
+// https://api.flutter.dev/flutter/widgets/ValueListenableBuilder-class.html
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _counter = ValueNotifier<int>(0);
+
+  void _incrementCounter() {
+    _counter.value++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (buildContext, value, child) {
+                return Column(children: [
+                  Text(
+                    '$value',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  child!
+                ]);
+              },
+              child: const Text('Good job!'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
